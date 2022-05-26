e014747146fc0a77c6c410c2054cd0f9e1a7516a
Lucas Alves <sorriso_colgati2711@hotmail.com>
2022-05-26 20:57:41 -0300

    routes links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { PublicarComponent } from './components/pages/publicar/publicar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sobre', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'publicar/new', component: PublicarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
