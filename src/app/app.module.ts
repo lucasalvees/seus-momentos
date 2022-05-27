eb09ef59737960bfec6d5687dd94b31cd61a6582
Lucas Alves <sorriso_colgati2711@hotmail.com>
2022-05-26 21:07:02 -0300

    change in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { PublicarComponent } from './components/pages/publicar/publicar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    PublicarComponent
   ],
   imports: [
     BrowserModule,
