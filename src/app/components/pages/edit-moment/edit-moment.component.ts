b07289dbc5620a9ee67126e2f46346e1442761c4
Lucas Alves <sorriso_colgati2711@hotmail.com>
2022-05-31 17:06:58 -0300

    create edit-moment page

---
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-moment',
+  templateUrl: './edit-moment.component.html',
+  styleUrls: ['./edit-moment.component.css']
+})
+export class EditMomentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
