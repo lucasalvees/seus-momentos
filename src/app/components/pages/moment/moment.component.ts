67480a608f0ad5c0c1000e47c480e56045c44736
Lucas Alves <sorriso_colgati2711@hotmail.com>
2022-06-01 00:24:05 -0300

    details page

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
+import { Moment } from 'src/app/Moment';
+import { environment } from 'src/environments/environment';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-moment',
@@ -6,10 +13,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./moment.component.css']
 })
 export class MomentComponent implements OnInit {
+  moment?: Moment;
+  baseApiUrl = environment.baseApiUrl;
+  faEdit = faEdit;
+  faTrash = faTrash;
 
-  constructor() { }
+  constructor(
+    private momentService: MomentService, 
+    private route: ActivatedRoute,
+    private messagesService: MessagesService,
+    private router: Router,
+    ) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.momentService.getMoment(id).subscribe((item) => (this.moment = item.data));
+  }
+
+  async removeHandler(id: number) {
+    await this.momentService.removeMoment(id).subscribe();
+
+    this.messagesService.add("Momento excluído com sucesso!");
+
+    this.router.navigate(['/'])
   }
 
 }
