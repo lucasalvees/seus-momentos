63378a2d29de772ea21007657ce7e5f7e0c654a0
Lucas Alves <sorriso_colgati2711@hotmail.com>
2022-06-01 13:16:07 -0300

    search bar tweaks

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  faSearch = faSearch;
+  searchTerm: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  search(event: Event) {
+    //console.log('Teste');
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) =>{
+      return moment.title.toLowerCase().includes(value);
+    });
   }
 
 }
